da742a7249e06eb1305e6cecd39e949aeaffa159
Mahmoud Jaber <12031959@students.liu.edu.lb>
2026-05-29 17:32:58 +0300

    Clean Apache MPM modules on startup

--- mobileapp/backend_php/start.sh
+++ mobileapp/backend_php/start.sh
@@ -3,7 +3,19 @@ set -e
 
 : "${PORT:=80}"
 
+rm -f /etc/apache2/mods-enabled/mpm_event.load \
+      /etc/apache2/mods-enabled/mpm_event.conf \
+      /etc/apache2/mods-enabled/mpm_worker.load \
+      /etc/apache2/mods-enabled/mpm_worker.conf \
+      /etc/apache2/mods-enabled/mpm_prefork.load \
+      /etc/apache2/mods-enabled/mpm_prefork.conf
+
+ln -s /etc/apache2/mods-available/mpm_prefork.load /etc/apache2/mods-enabled/mpm_prefork.load
+ln -s /etc/apache2/mods-available/mpm_prefork.conf /etc/apache2/mods-enabled/mpm_prefork.conf
+
 sed -i "s/^Listen .*/Listen ${PORT}/" /etc/apache2/ports.conf
 sed -i "s/<VirtualHost \*:.*/<VirtualHost *:${PORT}>/" /etc/apache2/sites-available/000-default.conf
+
+apache2ctl configtest
 
 exec apache2-foreground
